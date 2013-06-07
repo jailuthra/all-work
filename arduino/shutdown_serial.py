e6da02832b5b96812294daba82c784125f3595a6
Jai Luthra <me@jailuthra.in>
2013-06-07 13:44:34 +0530

    Modified shutdown_serial.py Waits for 3 seconds before shutting down.

--- arduino/shutdown_serial.py
+++ arduino/shutdown_serial.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python2
 
-import serial
-import os
+import serial, os, time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
     i = ser.read();
     if (i=='1'): 
-        #print "Hello, World"
+        print "Shutting down..."
+        time.sleep(3)
         os.system("shutdown -h now")
         exit()
 
