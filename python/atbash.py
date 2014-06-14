15cf98247554ebe39aa1f38bc2e0977ec809780e
Jai Luthra <me@jailuthra.in>
2014-06-14 17:41:48 +0530

    py: Deleted more old files. Modified atbash.

--- python/atbash.py
+++ python/atbash.py
@@ -2,15 +2,21 @@
 
 # Copyright (C) 2013 Jai Luthra <me@jailuthra.in> 
 
-str1 = input('Enter the String You Want To Code/Decode: ')
-str2 = ''
-for char in str1:
-    x = ord(char)
-    if x<=122 and x>=97: 
-        x = ( 27 - (x-96) ) + 96
-    elif x<=90 and x>=65: 
-        x = ( 27 - (x-64) ) + 64
-    char = chr(x)
-    str2 = str2 + char
-print ('Your Required String Is: '+ str2)
+def main():
+    str1 = input()
+    print (atbash(str1))
 
+def atbash(str1):
+    str2 = ''
+    for char in str1:
+        x = ord(char)
+        if x<=122 and x>=97: 
+            x = ( 27 - (x-96) ) + 96
+        elif x<=90 and x>=65: 
+            x = ( 27 - (x-64) ) + 64
+        char = chr(x)
+        str2 += char
+    return str2
+
+if __name__ == '__main__':
+    main()
