be39ff346c3ca55d0c0dc08bde4730f1d0e0d22a
Jai Luthra <me@jailuthra.in>
2015-05-29 18:42:52 +0530

    scheme: Change function definition syntax

--- functional_langs/scheme/bubble_sort.scm
+++ functional_langs/scheme/bubble_sort.scm
@@ -1,18 +1,14 @@
-(define sort
-  (lambda (x)
-    (cond ((null? x) x)
-          ((= (length x) 1) x)
-          ((<= (car x) (cadr x))
-           (check-again (cons (car x) (sort (cdr x)))))
-          (#t (check-again (cons (cadr x) (sort (cons (car x) (cddr x))))))
-    )
+(define (sort x)
+  (cond ((null? x) x)
+        ((= (length x) 1) x)
+        ((<= (car x) (cadr x))
+         (check-again (cons (car x) (sort (cdr x)))))
+        (#t (check-again (cons (cadr x) (sort (cons (car x) (cddr x))))))
   )
 )
 
-(define check-again
-  (lambda (x)
-    (if (<= (car x) (cadr x)) x
-        (sort x)
-    )
+(define (check-again x)
+  (if (<= (car x) (cadr x)) x
+      (sort x)
   )
 )
